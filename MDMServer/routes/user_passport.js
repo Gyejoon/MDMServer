7ae5b0856eaab0c3c9e7f2e4e94e3ecb780441b6
user <user@user-PC>
2017-11-05 15:08:04 +0900

    ss

--- MDMServer/routes/user_passport.js
+++ MDMServer/routes/user_passport.js
@@ -77,12 +77,7 @@ module.exports = function(app, passport) {
 			if(err){
 				throw err;
 			}
-<<<<<<< HEAD
 			res.json(result);
-=======
-			res.send(JSON.stringify({QR : result.QR_CODE,
-				CR : result.CR_CODE}));
->>>>>>> branch 'master' of https://github.com/Gyejoon/MDMServer
 		});
 	});
 };
